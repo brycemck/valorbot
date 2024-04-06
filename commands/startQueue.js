a070414a11a2b3d603c31ab5e45f5fbcab4d8b15
Bryce McKenney <brycemckenney@gmail.com>
2024-04-06 16:10:09 -0700

    got permission updating working and moved some of the code out of startQueue command and into the main process for cleanliness

--- commands/startQueue.js
+++ commands/startQueue.js
@@ -19,39 +19,11 @@ module.exports = {
     run: async (client, interaction, data) => {
       const that = module.exports;
 
-      const newQueue = data.games.newQueue()
-      try {
-        const newTextChannel = await interaction.channel.parent.children.create({
-          name: `queue-${newQueue.cleanId}`,
-          type: ChannelType.GuildText,
-          permissionOverwrites: [
-            {type: 'member', id: interaction.user.id, allow: [PermissionFlagsBits.ViewChannel]},
-            {type: 'role', id: interaction.guild.roles.everyone, deny: [PermissionFlagsBits.ViewChannel]}
-          ]
-        })
-        const newVoiceChannel = await interaction.channel.parent.children.create({
-          name: `queue-${newQueue.cleanId}`,
-          type: ChannelType.GuildVoice,
-          permissionOverwrites: [
-            {type: 'member', id: interaction.user.id, allow: [PermissionFlagsBits.ViewChannel]},
-            {type: 'role', id: interaction.guild.roles.everyone, deny: [PermissionFlagsBits.ViewChannel]}
-          ]
-        })
-        that.embeddedMessage.fields = []     
-        that.embeddedMessage.fields.push({name: `Queue #${newQueue.cleanId} Open`, value: ''})
+      const newQueue = await data.games.newQueue(interaction)
+      that.embeddedMessage.fields = []     
+      that.embeddedMessage.fields.push({name: `Queue #${newQueue.cleanId} opened, and you've been added to it. View game information and join VC with your queuemates in queue-${newQueue.cleanId}.`, value: ''})
 
-        const queueJoiner = new ButtonBuilder()
-          .setCustomId(`queue-${newQueue.cleanId}`)
-          .setLabel('Join the queue')
-          .setStyle(1)
-  
-        const row = new ActionRowBuilder().addComponents(queueJoiner)
-        return interaction.reply({embeds: [that.embeddedMessage], components: [row]})
-      } catch (error) {
-        that.embeddedMessage.fields = []     
-        that.embeddedMessage.fields.push({name: `Failure to start new queue`, value: 'Something went wrong, sorry there bud'})
-        interaction.reply({embeds: [that.embeddedMessage]})
-      }
+      return interaction.reply({embeds: [that.embeddedMessage], ephemeral: true})
       // console.log(interaction)
     }
   }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, Routes, REST } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, Routes, REST, PermissionOverwrites, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder, ChannelType } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -101,11 +101,18 @@ const buttonHandler = async (interaction) => {
   const buttonId = interaction.customId.split('-')
   const action = buttonId[0]
   const gameId = buttonId[1]
+  const thisGameIndex = savedData.games.Games.findIndex(game => game.cleanId == gameId);
+  const thisGame = savedData.games.Games[thisGameIndex]
+  
   if (action == 'queue') {
-    savedData.games.addPlayerToQueue(interaction, gameId)
-    savedData.games.save()
+    if (!thisGame.players.includes(interaction.user.id)) {
+      savedData.games.addPlayerToQueue(interaction, gameId)
+      savedData.games.save()
 
-    return interaction.reply({content: `You're in the queue for #${gameId}. View game information and join VC with your queuemates in queue-${gameId}.`, ephemeral: true})
+      return interaction.reply({content: `You're in the queue for #${gameId}. View game information and join VC with your queuemates in queue-${gameId}.`, ephemeral: true})
+    } else {
+      return interaction.reply({content: `You're already in the queue for #${gameId}, silly.`, ephemeral: true})
+    }
   }
 } 
 const initStores = async () => {
@@ -149,7 +156,9 @@ const savedData = {
       defenders: [],
       attackers: [],
       map: '',
-      state: '' // active | completed | queue
+      state: '', // active | completed | queue
+      voiceChannelId: '',
+      textChannelId: ''
     },
     Games: [],
     save: () => {
@@ -166,7 +175,7 @@ const savedData = {
     generateId: () => {
       return [crypto.randomUUID(), `${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`]
     },
-    newQueue: () => {
+    newQueue: async (interaction) => {
       let thisQueue = Object.assign({}, savedData.games.schema)
       const newId = savedData.games.generateId()
 
@@ -175,19 +184,80 @@ const savedData = {
       thisQueue.timestamp = Date.now()
       thisQueue.state = 'queue'
 
+      try {
+        const newTextChannel = await interaction.channel.parent.children.create({
+          name: `queue-${thisQueue.cleanId}`,
+          type: ChannelType.GuildText,
+          permissionOverwrites: [
+            {type: 'member', id: client.user.id, allow: [PermissionFlagsBits.ViewChannel]}, // add Valorbot to the channel so it can manage it
+            {type: 'role', id: interaction.guild.roles.everyone, deny: [PermissionFlagsBits.ViewChannel]} // deny @everyone
+          ]
+        })
+        const newVoiceChannel = await interaction.channel.parent.children.create({
+          name: `queue-${thisQueue.cleanId}`,
+          type: ChannelType.GuildVoice,
+          permissionOverwrites: [
+            {type: 'member', id: client.user.id, allow: [PermissionFlagsBits.ViewChannel]}, // add Valorbot to the channel so it can manage it
+            {type: 'role', id: interaction.guild.roles.everyone, deny: [PermissionFlagsBits.ViewChannel]} // deny @everyone
+          ]
+        })
+        thisQueue.voiceChannelId = newVoiceChannel.id
+        thisQueue.textChannelId = newTextChannel.id
+      } catch (error) {
+
+      }
+
       savedData.games.Games.push(thisQueue)
       savedData.games.save()
 
+      // add player to the queue
+      savedData.games.addPlayerToQueue(interaction, thisQueue.cleanId)
+
+      // send message to Queues channel with new queue
+      const queueJoiner = new ButtonBuilder()
+          .setCustomId(`queue-${thisQueue.cleanId}`)
+          .setLabel('Join the queue')
+          .setStyle(1)
+  
+      const row = new ActionRowBuilder().addComponents(queueJoiner)
+
+      let embeddedMessage = {}
+      embeddedMessage.fields = [{name: `Queue #${thisQueue.cleanId} opened!`, value: ''}]
+      const queueOpenedMessage = await interaction.channel.send({embeds: [embeddedMessage], components: [row]})
+      thisQueue.queueMessageId = queueOpenedMessage.id
+      
       return thisQueue
     },
-    addPlayerToQueue: (interaction, id) => {
-      const thisGameIndex = savedData.games.Games.findIndex((game) => game.cleanId == id)
+    addPlayerToQueue: async (interaction, id) => {
+      console.log(`adding ${interaction.user.username} to queue ${id}`)
+      const thisGameIndex = savedData.games.Games.findIndex(game => game.cleanId == id);
       const thisGame = savedData.games.Games[thisGameIndex]
 
+      // add player id to players array in game object
       thisGame.players.push(interaction.user.id)
+      
+      // get text and voice channels for this queue
+      interaction.guild.channels.fetch()
+      const queueTextChannel = await interaction.guild.channels.fetch(thisGame.textChannelId)
+      const queueVoiceChannel = await interaction.guild.channels.fetch(thisGame.voiceChannelId)
+      
+      // add permissions to both cnhannels
+      queueTextChannel.permissionOverwrites.edit(interaction.user.id, {'ViewChannel': true})
+      queueVoiceChannel.permissionOverwrites.edit(interaction.user.id, {'ViewChannel': true})
 
-      let channels = interaction.guild.channels.cache.find(channel => channel.name == `queue-${id}`)
-      console.log(channels)
+      // send message to text channel that player joined
+      let embeddedMessage = {}
+      embeddedMessage.fields = [{name: `Queue`, value: `Player <@${interaction.user.id}> has joined the queue!`}]
+      queueTextChannel.send({embeds: [embeddedMessage]})
+ 
+      // start the game once we get a full queue
+      if (thisGame.players.length == 3) {
+        savedData.games.startGame(thisGame)
+      }
+      savedData.games.save()
+    },
+    startGame: (thisGame) => {
+      console.log('starting game!!!!')
     }
   },
   init: () => {
